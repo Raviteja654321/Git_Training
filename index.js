36890cf29a2a152f08989f0ce7fc1e1cc2eee4c9
Raviteja654321 <raviteja.nagulapalli@students.iiit.ac.in>
2024-05-20 12:13:45 +0530

    added functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let str1="hello"
 let str2=" temp"
 
-let str3= `${str} raviteja`
+let str3= `${str1} raviteja`
 
-console.log(str3)
+console.log(str3);
+
+let output = ()=> {return 2+5}
+
+console.log(output())
